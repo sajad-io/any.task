38fac5c89a82a474077d71b99502341cd5445c52
Sajad Rahmdel <me@sajad.io>
2022-11-15 21:01:33 +0900

    my tasks page > finished UI

--- lib/my_tasks_page/my_tasks_page.dart
+++ lib/my_tasks_page/my_tasks_page.dart
@@ -2,33 +2,34 @@ import 'package:any_task/my_tasks_page/task_list_tile_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../add_task_page/add_task_page.dart';
 import '../models/task_model.dart';
+
+// dummy task data
+final List<Task> myTasks = [
+  Task(
+    title: 'Study Flutter with Sajad :)',
+    date: DateTime(2022, 10, 12),
+    startTime: TimeOfDay(hour: 10, minute: 30),
+    endTime: TimeOfDay(hour: 11, minute: 30),
+    isDone: true,
+  ),
+  Task(
+    title: 'Meeting with Client',
+    date: DateTime(2022, 11, 12),
+    startTime: TimeOfDay(hour: 14, minute: 30),
+    endTime: TimeOfDay(hour: 15, minute: 0),
+  ),
+  Task(
+    title: 'Daily Project Collab',
+    date: DateTime(2022, 12, 13),
+    startTime: TimeOfDay(hour: 9, minute: 30),
+    endTime: TimeOfDay(hour: 17, minute: 45),
+  ),
+];
 
 class MyTasksPage extends StatefulWidget {
   MyTasksPage({super.key});
-
-  // dummy task data
-  final List<Task> myTasks = [
-    Task(
-      title: 'Study Flutter with Sajad :)',
-      date: DateTime(2022, 10, 12),
-      startTime: TimeOfDay(hour: 10, minute: 30),
-      endTime: TimeOfDay(hour: 11, minute: 30),
-      isDone: true,
-    ),
-    Task(
-      title: 'Meeting with Client',
-      date: DateTime(2022, 11, 12),
-      startTime: TimeOfDay(hour: 14, minute: 30),
-      endTime: TimeOfDay(hour: 15, minute: 0),
-    ),
-    Task(
-      title: 'Daily Project Collab',
-      date: DateTime(2022, 12, 13),
-      startTime: TimeOfDay(hour: 9, minute: 30),
-      endTime: TimeOfDay(hour: 17, minute: 45),
-    ),
-  ];
 
   @override
   State<MyTasksPage> createState() => _MyTasksPageState();
@@ -53,13 +54,13 @@ class _MyTasksPageState extends State<MyTasksPage> {
       ),
       body: ListView.separated(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 19),
-        itemCount: widget.myTasks.length,
+        itemCount: myTasks.length,
         itemBuilder: (BuildContext context, int index) {
           return TaskListTileWidget(
-            task: widget.myTasks[index],
+            task: myTasks[index],
             onCheckboxChanged: (isChecked) {
               setState(() {
-                widget.myTasks[index].isDone = isChecked!;
+                myTasks[index].isDone = isChecked!;
               });
             },
           );
@@ -67,6 +68,13 @@ class _MyTasksPageState extends State<MyTasksPage> {
         separatorBuilder: (BuildContext context, int index) {
           return SizedBox(height: 12);
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddTaskPage()));
+        },
+        backgroundColor: Color(0xFF4376FE),
+        child: const Icon(Icons.add),
       ),
     );
   }
