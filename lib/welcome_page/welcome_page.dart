5e0a2a973c1faacf6a6b30d8f1d76e4874c9e30d
Sajad Rahmdel <me@sajad.io>
2022-11-15 13:45:13 +0900

    welcome page UI finished

---
+++ lib/welcome_page/welcome_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF29333E),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 61),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Checkbox Icon
+                  Image.asset(
+                    'assets/icons/ic_app_logo.png',
+                    height: 80,
+                    width: 80,
+                  ),
+                  // Empty space between
+                  SizedBox(
+                    height: 24,
+                  ),
+                  // any.task text
+                  Text(
+                    'any.task',
+                    style: GoogleFonts.poppins(
+                      fontSize: 60,
+                      color: Color(0xFFFFFFFF),
+                    ),
+                  ),
+                  // Empty space between
+                  SizedBox(
+                    height: 24,
+                  ),
+                  // Get any task done with any.task
+                  Text(
+                    'Get any task done with any.task',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFFFFFFFF),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 65,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Get Started',
+                  style: GoogleFonts.roboto(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+              ),
+            ),
+            // Empty space between
+            SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
